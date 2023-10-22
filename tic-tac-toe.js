1088c23dc838c2269f894a3cc79d6d54cd95d454
Brittany Thomas <yaneelthomas162@gmail.com>
2023-10-22 16:07:03 -0500

    Second commit of tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,36 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    var boxes = document.querySelectorAll("#board div");
-    boxes.forEach(function(elem, index) {
+    var squares = document.querySelectorAll("#board div");
+    var isPlayerX = true;
+    var gameState = ["0","1","2","3","4","5","6","7","8"];
+
+    squares.forEach(function(elem,index) {
+
         elem.setAttribute('class', 'square');
+        elem.addEventListener("click", function () 
+        {
+            if (elem.textContent=="") {
+                if (isPlayerX) {
+                    elem.textContent = "X";
+                    elem.classList.add("X");
+                    gameState[index]="X";
+                    console.log(gameState)
+                } else {
+                    elem.textContent = "O";
+                    elem.classList.add("O");
+                    gameState[index]="O";
+                    console.log(gameState)
+                }
+    
+                isPlayerX = !isPlayerX;
+            }
+    
+        });
     });
+  
+
+
 
   });
 
